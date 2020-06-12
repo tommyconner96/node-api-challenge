fe66ef1d5dcfdf24be724ae0bdb314f6b963a4d7
unknown <tommyconner96@gmail.com>
2020-06-12 19:30:12 -0400

    server runs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+//imports
+const express = require("express")
+// const projectRouter = require("./projects/project-router")
+// const actionRouter = require("./actions/action-router")
+// const welcomeRouter = require("./welcome/welcome-router")
 
-Sing along:
+//express server
+const server = express()
+const port = 6666
+server.use(express.json())
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+//routes
+// server.use('/', welcomeRouter)
+// server.use('/projects', projectRouter)
+// server.use('/projects/:id/actions', actionRouter)
 
-Go code!
-*/
+//server listening
+server.listen(port, () => {
+    console.log(`server is listening at port: ${port}`)
+  })
